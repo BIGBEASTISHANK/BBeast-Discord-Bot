e9e8119788a6c7f3e49011b3dece5af008afabef
BIGBEASTISHANK <antihack@bigbeastishank.com>
2023-12-17 02:16:03 +0530

    remove: ban command

--- src/main.cpp
+++ src/main.cpp
@@ -42,25 +42,13 @@ int main()
               .set_color(bbGlobalVariable::EMBED_COLOR)
               .set_title("Anonymous Confession!")
               .set_description(paraMsg)
-              // .set_footer("Time: " + bbFunc::CurrentDateTime(), "");
               .set_timestamp(bbGlobalVariable::CurrentTime);
 
       // Sending msg
       dpp::message msg(event.command.channel_id, embed);
       event.reply("");
       bot.message_create(msg);
-    }
-
-    // Ban command
-    else if (event.command.get_command_name() == "ban") {
-      // Variables
-      string reason = get<string>(event.get_parameter("reason"));
-      dpp::snowflake userTB = get<dpp::snowflake>(event.get_parameter("user"));
-      uint32_t temp;
-      bot.guild_ban_add(event.command.guild_id, userTB, temp);
-      cout<<temp;
-      event.reply("banned");
-    } });
+    }});
 
   // On ready event
   bot.on_ready([&bot](const dpp::ready_t &event)
@@ -77,17 +65,8 @@ int main()
           dpp::co_string, "message",
           "Give a message to send it anonymously in this channel!", true));
 
-      // Ban
-      dpp::slashcommand ban("ban", "Slowdown chat", bot.me.id);
-      ban.set_default_permissions(
-          dpp::p_ban_members); // checking for permission
-      ban.add_option(
-          dpp::command_option(dpp::co_user, "user", "User to ban", true));
-      ban.add_option(
-          dpp::command_option(dpp::co_string, "reason", "reason to ban", true));
-
       bot.global_bulk_command_create(
-          {ping, confess, ban}); // Creating slash command
+          {ping, confess}); // Creating slash command
     };
 
     // Outputing when bot is ready
