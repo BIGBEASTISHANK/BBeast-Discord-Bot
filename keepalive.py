c94b4813da1ca2d4fc6087dd50ec762891376bbc
BIGBEASTISHANK <antihack@bigbeastishank.com>
2023-12-24 08:23:28 +0530

    changed: keepalive webpage view

--- keepalive.py
+++ keepalive.py
@@ -5,7 +5,7 @@ app = Flask('')
 @app.route('/')
 
 def home():
-    return "Dont look dumb ass, trying to hack my id, goto hell"
+    return "<a href='https://bigbeastishank.com'>View owner's website</a>"
 
 def run():
   app.run(host='0.0.0.0',port=8080)
